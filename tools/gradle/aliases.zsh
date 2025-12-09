f4ce0fe6f13bc3e6590105c1d9269e75845fad36
druchtx <xulu@ymail.ne.jp>
2025-12-09 23:01:35 +0900

    add gradle settings

--- tools/gradle/aliases.zsh
+++ tools/gradle/aliases.zsh
@@ -3,14 +3,11 @@
 function gradlew() {
   local gradlew_path=$(find . -name 'gradlew' -type f -maxdepth 1)
 
-  if [ ! -x "$gradlew_path" ]; then
-    echo "No execute permission for $gradlew_path" >&2
-    return 1
-  elif [ ! -f "$gradlew_path" ]; then
+  if [ ! -f "$gradlew_path" ]; then
     echo "No gradlew found in current directory" >&2
     return 1
-  else
-    $gradlew_path "$@"
   fi
+  $gradlew_path "$@"
 }
+
 
--- tools/gradle/path.zsh
+++ tools/gradle/path.zsh
@@ -1,12 +1,4 @@
 #!/bin/zsh
 
-function grdl() {
-  local gradlew_path=$(find . -name 'gradlew' -type f -maxdepth 1)
-  if [ -n "$gradlew_path" ]; then
-    $gradlew_path "$@"
-  else
-    echo "No gradlew found in current directory" >&2
-    return 1
-  fi
-}
-
+# Default settings for gradle global configurations.
+export GRADLE_USER_HOME="$HOME/.gradle"
