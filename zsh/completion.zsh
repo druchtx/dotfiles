9acae451eae7723da49d5fe7ea10fb8f5d456483
druchtx <xulu@ymail.ne.jp>
2026-01-10 17:29:01 +0900

    sync changes

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -4,9 +4,9 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
+# use arrow keys to navigate the list instead of just tab cycling
 zstyle ':completion:*:*:*:*:*' menu select
 
 # Added for message and description display
 zstyle ':completion:*:messages' format '%d'
-# zstyle ':completion:*:descriptions' format '%d'
-# zstyle ':completion:*:warnings' format '--- no matches ---'
+zstyle ':completion:*:warnings' format '%F{red}-- no matches found --%f'
