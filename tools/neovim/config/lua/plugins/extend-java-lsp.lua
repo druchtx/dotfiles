78ab52d503a0feb261609b627b975c029cab64cb
druchtx <xulu@ymail.ne.jp>
2026-06-14 14:19:51 +0900

    chore(nvim): add java lsp

--- tools/neovim/config/lua/plugins/extend-java-lsp.lua
+++ tools/neovim/config/lua/plugins/extend-java-lsp.lua
@@ -1,10 +1,14 @@
--- define a specific jdk for jdtls
-local jdtls_java = "/Users/druchtx/.local/share/mise/installs/java/corretto-21.0.10.7.1/bin/java"
+local jdtls_runtime = "corretto-21"
 
 return {
   {
     "mfussenegger/nvim-jdtls",
     opts = function(_, opts)
+      local java_home = vim.trim(vim.fn.system({ "mise", "where", "java@" .. jdtls_runtime }))
+      local jdtls_java = (vim.v.shell_error == 0 and java_home ~= "" and java_home .. "/bin/java")
+        or (vim.env.JAVA_HOME and vim.env.JAVA_HOME .. "/bin/java")
+        or "java"
+
       if vim.fn.executable(jdtls_java) ~= 1 then
         vim.schedule(function()
           vim.notify("Configured jdtls Java not found: " .. jdtls_java, vim.log.levels.ERROR)
