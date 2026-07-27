9ff6ff66a05d84091fb1c2bbe4b4ea677918c6c4
druchtx <xulu@ymail.ne.jp>
2026-07-28 08:01:08 +0900

    fix(hammerspoon): restore proportional centered window size

--- tools/hammerspoon/modules/window.lua
+++ tools/hammerspoon/modules/window.lua
@@ -131,8 +131,9 @@ local windowPositions = {
 		saveFrame(win)
 
 		-- Set desired window size
-		local targetWidth = screen.w * 0.85
-		local targetHeight = screen.h * 0.93
+		local targetWidth = screen.w * 0.86875
+		local targetHeight = screen.h * 0.952381
+
 		-- Calculate position to center the window
 		local centerX = screen.x + (screen.w - targetWidth) / 2
 		local centerY = screen.y + (screen.h - targetHeight) / 2
